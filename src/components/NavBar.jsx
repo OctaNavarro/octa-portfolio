27bdca409241bfa83e85c4b62b94d1d1f8368593
navarrooctavioa@gmail.com <navarrooctavioa@gmail.com>
2023-02-01 17:30:04 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -50,13 +50,13 @@ export default function () {
      
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10  md:hidden"
+        className="cursor-pointer pr-4 z-20  md:hidden"
       >
         {nav ? <FaTimes size={25} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-dblue to-blue">
+        <ul className="flex z-10 flex-col justify-center items-center absolute top-0 left-0 w-full h-[110vh] bg-gradient-to-b from-dblue to-blue">
           {links.map(({ id, link }) => (
             <li
               key={id}
